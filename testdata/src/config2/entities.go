cef89cd79c66b3fbb71d70c6d78297a946b438aa
Lubomír Cvrk <38976574+digitalstraw@users.noreply.github.com>
2025-12-07 18:17:55 +0100

    Fix: Update go module version. (#22)

--- testdata/src/config2/entities.go
+++ testdata/src/config2/entities.go
@@ -1,6 +1,6 @@
 package config2
 
-import "github.com/digitalstraw/propro/testdata/src/protectselected"
+import "github.com/digitalstraw/propro/v2/testdata/src/protectselected"
 
 var EntityList = []any{
 	&protectselected.Entity{},
