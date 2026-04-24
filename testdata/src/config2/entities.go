3321fc4aff32d975496458f9f94cc737e9559ffd
Lubomir Cvrk <digitalstraw@gmail.com>
2026-04-24 18:28:43 +0200

    Fix: Update propro major version in code.

--- testdata/src/config2/entities.go
+++ testdata/src/config2/entities.go
@@ -1,6 +1,6 @@
 package config2
 
-import "github.com/digitalstraw/propro/v2/testdata/src/protectselected"
+import "github.com/digitalstraw/propro/v3/testdata/src/protectselected"
 
 var EntityList = []any{
 	&protectselected.Entity{},
